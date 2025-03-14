7b0e7e53c0ae360013d51b878d4101c67748df03
Victor Lorencio <victorlorencio@gmail.com>
2025-03-14 20:10:28 +0100

    Update Cors

--- src/index.js
+++ src/index.js
@@ -90,6 +90,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));  // Usamos cors con las opciones definidas
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // 🔹 Permite todos los dominios
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
+
 app.use(workoutsRouter);  // Usamos las rutas de workouts
 app.use(usersRouter);  // Usamos las rutas de users
 
